b2450132bf0798fe9e3db92819ab7362c4a5582f
devance <dj788@snu.edu.in>
2024-05-22 19:48:06 +0530

    puts back boilerpalte code

--- frontend/src/components/code-editor.jsx
+++ frontend/src/components/code-editor.jsx
@@ -3,18 +3,20 @@
 import Editor from "@monaco-editor/react";
 import { useState, useRef, useEffect } from "react";
 import { LangSelect } from "@/components/lang-select";
-// import BOILERPLATE from "@/contants";
+import BOILERPLATE from "@/contants";
 import Output from "./output";
 import Prop from "prop-types";
 const CodeEditor = ({ socketRef, roomId, onCodeChange, onLanguageChange }) => {
   const editorRef = useRef(null);
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState(BOILERPLATE["python"]);
   const [language, setLanguage] = useState("python");
-  // const isTypingLocal = useRef(false);
+
   const onLangChange = (value) => {
-    // const newCode = BOILERPLATE[value];
+    const newCode = BOILERPLATE[value];
     onLanguageChange(value);
+    setCode(newCode);
     socketRef.current.emit("language-change", { roomId, language: value });
+    socketRef.current.emit("code-change", { roomId, code: newCode });
     setLanguage(value);
   };
 
@@ -29,14 +31,11 @@ const CodeEditor = ({ socketRef, roomId, onCodeChange, onLanguageChange }) => {
   };
 
   useEffect(() => {
-    // console.log("roomId", roomId);
-    // console.log("socketRef", socketRef);
     if (socketRef.current) {
       socketRef.current.on("code-change", ({ code }) => {
         if (code !== null) {
           setCode(code);
           onCodeChange(code);
-          // editorRef.current?.setValue(code);
         }
       });
       socketRef.current.on("language-change", ({ language }) => {
