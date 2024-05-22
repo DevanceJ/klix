d495e4b3ea0cdfd6750b9e593b1dfb93f24b5ff6
yathansh13 <yathansh1312@gmail.com>
2024-05-22 06:23:20 +0530

    Added dynamic support for language versions

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,19 +1,37 @@
 import axios from "axios";
-import { LANGUAGE_VERSIONS } from "./contants";
 
 const API = axios.create({
   baseURL: "https://emkc.org/api/v2/piston",
 });
 
+export const fetchLanguageVersions = async () => {
+  const response = await API.get("/runtimes");
+  const data = response.data;
+
+  const languageVersions = {};
+  data.forEach((item) => {
+    languageVersions[item.language] = item.version;
+  });
+
+  console.log(languageVersions);
+  return languageVersions;
+};
+
 export const executeCode = async (language, sourceCode) => {
+  const languageVersions = await fetchLanguageVersions();
+
+  // Check if the language is cpp and change it to c++
+  const languageToUse = language === "cpp" ? "c++" : language;
+
   const response = await API.post("/execute", {
-    language: language,
-    version: LANGUAGE_VERSIONS[language],
+    language: languageToUse,
+    version: languageVersions[languageToUse],
     files: [
       {
         content: sourceCode,
       },
     ],
   });
+
   return response.data;
 };
